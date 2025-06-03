8cb502cf705975a5bd8da12d77cc6e3300ef4621
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-03 18:26:05 +0700

    Membuat class pekerja yang memiliki akses ke class orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -9,6 +9,13 @@ class orang{
     {
         cout<< "Orang dibuat dengan umur "<< umur << "\n" <<endl;
     }
+};
 
-
+class pekerja : virtual public orang{
+    public:
+    pekerja(int pUmur) :
+        orang (pUmur)
+    {
+        cout<< "pekerja dibuat\n" <<endl;
+    }
 };
