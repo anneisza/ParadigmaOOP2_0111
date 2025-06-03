cafb5634f0d23762faace9cf6fb92776e332fa31
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-03 18:14:50 +0700

    Mengisi member class Remote Lampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 class remoteLampu{
+    private:
+    string skalarNo[10];
+    public:
+    void setSaklarNo(int i, string value){
+        skalarNo[i] = value;
+    }
+    string getSaklarNo(int i){
+        return skalarNo[i];
+    }
 
 };
