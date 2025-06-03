35c2b44d1b235bbe3e721481312f6946ddfdc994
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-03 17:45:19 +0700

    Merapikan file polimorpi 2

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 using namespace std;
 
-class seseorang{
+class seseorang
+{
 public:
     virtual void pesan() = 0;
-    //virtual void pesan(){
-    // cout<< "Pesan dari seseorang"<<endl;
-    //}
+    // virtual void pesan(){
+    //  cout<< "Pesan dari seseorang"<<endl;
+    // }
 };
 
-class joko : public seseorang{
+class joko : public seseorang
+{
 public:
-    void pesan(){
-        cout<< "Pesan dari Joko"<<endl;
-    }
-
-};
-
-class lia : public seseorang{
-public:
-    void pesan(){
-        cout<< "Pesan dari Lia"<<endl;
+    void pesan()
+    {
+        cout << "Pesan dari Joko" << endl;
     }
 };
 
-int main(){
-    seseorang*obyek;
+class lia : public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari Lia" << endl;
+    }
+};
+
+int main()
+{
+    seseorang *obyek;
     joko a;
     lia b;
 
@@ -33,8 +38,7 @@ int main(){
     obyek->pesan();
     obyek = &b;
     obyek->pesan();
-    //a.seseorang::pesan();
-    //b.seseorang::pesan();
+    // a.seseorang::pesan();
+    // b.seseorang::pesan();
     return 0;
-
 }
