65279ae0565308cb665caa5dffde963a5cc7a16b
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-03 17:56:20 +0700

    Merapikan file abstraksi

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 
-class AbstraksiKlas{
-    private:
+class AbstraksiKlas
+{
+private:
     string x, y;
 
-    public:
+public:
+    // method untuk mengisi nilai
+    // private member
 
-    //method untuk mengisi nilai
-    //private member
-
-    void setXY(string a, string b){
+    void setXY(string a, string b)
+    {
         x = a;
         y = b;
     }
-    //menampilkan nilai
-    void display(){
-        cout<< "x = "<< x << endl;
-        cout<< "y = "<< y << endl;
+    // menampilkan nilai
+    void display()
+    {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
     }
 };
 
-int main(){
+int main()
+{
     AbstraksiKlas ak;
     ak.setXY("Yogyakarta", "Kampus");
     ak.display();
