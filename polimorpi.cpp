a2b86c16ece196d162d655ccf5f28bde5cfb3acc
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-03 17:26:28 +0700

    Menambahkan class seseorang

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class seseorang{
+    public:
+    virtual void pesan() = 0;
+    //virtual void pesan(){
+    // coutr<< "Pesan dari seseorang"<<endl;
+    //}
+};
+
+class joko : public seseorang{
+
+};
